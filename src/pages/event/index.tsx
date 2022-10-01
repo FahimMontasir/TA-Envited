14223410b013fa6ad9d9fdf01d40eb0f180590cb
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-10-02 01:49:33 +0600

    done

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -3,13 +3,13 @@ import "./event.css";
 
 function Event() {
   return (
-    <main className="event bg-neutrals-white-100 flex flex-col items-center">
+    <main className="event bg-neutrals-white-100 flex flex-col items-center xl:flex-row">
       <img
-        className="event__image"
+        className="w-[100%] h-[375px] sm:w-[593px] sm:h-[593px] md:mt-16 xl:order-2"
         src="/Birthday-cake.png"
         alt="envited-event"
       />
-      <div className="event__title py-2 text-left mt-2 ">
+      <div className="event__title py-2 text-left mt-2 xl:order-1">
         <h1 className="event__title-name text-primary-blue">Birthday Bash</h1>
         <p className="event__title-host text-neutrals-gray-300">
           Hosted by Elysia
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -4,26 +4,26 @@ import "./landing.css";
 function HomePage() {
   const navigate = useNavigate();
   return (
-    <main className="landing bg-secondaryPurples-100 flex flex-col items-center justify-between">
-      <section className="landing__hero">
-        <div className="landing__hero-heading text-center">
+    <main className="landing bg-secondaryPurples-100 flex flex-col items-center justify-between xl:flex-row">
+      <section className="landing__hero xl:order-2 xl:mb-5 xl:mr-28">
+        <div className="landing__hero-heading text-center xl:text-right">
           <h1 className="text-primary-blue">Imagine if</h1>
           <h1 className="landing__hero-snap">Snapchat</h1>
           <h1 className="text-primary-blue">had events.</h1>
         </div>
-        <p className="landing__hero-sub text-center mt-3 text-neutrals-gray-400">
+        <p className="landing__hero-sub text-center xl:text-right mt-3 text-neutrals-gray-400">
           Easily host and share events with your friends across any social
           media.
         </p>
       </section>
       <img
-        className="landing__image mt-10"
+        className="landing__image mt-10 w-[165.63px] h-[292px] sm:w-[60%] sm:h-[60%] xl:order-1"
         src="/Landing-page-image.svg"
         alt="envited-landing"
       />
       <button
         onClick={() => navigate("/create")}
-        className="landing__button text-neutrals-white-100 mb-5"
+        className="landing__button text-neutrals-white-100 mb-5 w-[187px] sm:w-[40%] xl:w-[20%] xl:order-2 xl:absolute xl:right-28 xl:bottom-40"
       >
         🎉 Create my event
       </button>
